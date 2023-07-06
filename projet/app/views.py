263270483b6db613088858ba50a76a3a39850ab2
Kévin Vds <118352151+SiskaKizuato@users.noreply.github.com>
2023-07-06 10:05:08 +0200

    creation et fin conversion basique views et url

--- projet/app/views.py
+++ projet/app/views.py
@@ -1,5 +1,39 @@
 from django.shortcuts import render
 
 # Create your views here.
-def home(request):
-    return render(request, 'app/home.html')
+def index(request):
+    return render(request, 'app/front/main/index.html')
+
+def blog5(request):
+    return render(request, 'app/front/main/blog-5.html')
+
+def cart(request):
+    return render(request, 'app/front/main/cart.html')
+
+def checkout(request):
+    return render(request, 'app/front/main/checkout.html')
+
+def contact(request):
+    return render(request, 'app/front/main/contact.html')
+
+def error404(request):
+    return render(request, 'app/front/main/error-404.html')
+
+def login(request):
+    return render(request, 'app/front/main/login.html')
+
+def productLeftSideBar2(request):
+    return render(request, 'app/front/main/product-left-sidebar-2.html')
+
+def productsType1(request):
+    return render(request, 'app/front/main/products-type-1.html')
+
+def signup(request):
+    return render(request, 'app/front/main/signup.html')
+
+def singleBlog1(request):
+    return render(request, 'app/front/main/single-blog-1.html')
+
+def trackOrder(request):
+    return render(request, 'app/front/main/track-order.html')
+
--- projet/projet/urls.py
+++ projet/projet/urls.py
@@ -19,6 +19,16 @@ from django.urls import path
 from app import views
 
 urlpatterns = [
-    # path('admin/', admin.site.urls),
-    path('', views.home),
+    path('', views.index, name="index"),
+    path('blog-5/', views.blog5, name="blog5"),
+    path('cart/', views.cart, name="cart"),
+    path('checkout/', views.checkout, name="checkout"),
+    path('contact/', views.contact, name="contact"),
+    path('error-404/', views.error404, name="error404"),
+    path('login/', views.login, name="login"),
+    path('product-left-sidebar-2/', views.productLeftSideBar2, name="productLeftSideBar2"),
+    path('products-type-1/', views.productsType1, name="productsType1"),
+    path('signup/', views.signup, name="signup"),
+    path('single-blog-1/', views.singleBlog1, name="singleBlog1"),
+    path('track-order/', views.trackOrder, name="trackOrder"),
 ]
