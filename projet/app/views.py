326571341ad3032042bcd55620ec6cb9fc0dd83c
Kévin Vds <118352151+SiskaKizuato@users.noreply.github.com>
2023-07-10 12:21:03 +0200

    fix login

--- projet/app/views.py
+++ projet/app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.hashers import make_password
 from .forms import SignupForm
 from .models import Profile# Create your views here.
 # XXXXX FRONT XXXXX
@@ -21,19 +22,7 @@ def contact(request):
 def error404(request):
     return render(request, 'app/front/main/error-404.html')
 
-def login_view(request):
-    if request.method == "POST":
-        username = request.POST['username']
-        password = request.POST['password']
-        user = authenticate(request, username=username, password=password)
-        if user is not None:
-            login(request, user)
-            return redirect('index')
-        else:
-            error_message = "Nom d'utilisateur ou mot de passe incorrect."
-            return render(request, 'app/front/main/login.html',{'error_message':error_message})
-    else:
-        return render(request, 'app/front/main/login.html')
+
 
 def productLeftSideBar2(request):
     return render(request, 'app/front/main/products-left-sidebar-2.html')
@@ -47,6 +36,7 @@ def signup(request):
         if form.is_valid():
             user = form.save(commit=False)
             user.role = 'member'  # Définir le rôle de l'utilisateur en tant que membre
+            user.password=make_password(form.cleaned_data['password'])
             user.save()
             login(request,user)
             return redirect('index')  # Rediriger vers la page d'accueil après l'inscription
@@ -54,6 +44,20 @@ def signup(request):
     else:
         form = SignupForm()
     return render(request, 'app/front/main/signup.html', {'form': form})
+
+def login_view(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('index')
+        else:
+            error_message = "Nom d'utilisateur ou mot de passe incorrect."
+            return render(request, 'app/front/main/login.html', {'error_message': error_message})
+    else:
+        return render(request, 'app/front/main/login.html')
 
 def logout_view(request):
     logout(request)
